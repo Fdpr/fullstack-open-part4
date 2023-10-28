e23290fa8da569305c420a32636c77004e7183c5
Florian <70607158+Fdpr@users.noreply.github.com>
2023-10-29 00:14:46 +0300

    4.7

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -175,3 +175,209 @@ describe("favorite blog", () => {
         })
     })
 })
+
+describe("most blogs", () => {
+    test("of an empty list", () => {
+        expect(listHelper.mostBlogs([])).toEqual({})
+    })
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    test("of a list with one entry", () => {
+        expect(listHelper.mostBlogs(listWithOneBlog)).toEqual({
+            author: listWithOneBlog[0].author,
+            blogs: 1
+        })
+    })
+
+    const listWithMultiples = [
+        {
+            "_id": "653d623e6c9f6b487612fd23",
+            "title": "I am test blog",
+            "author": "Mister Minit",
+            "url": "localhost",
+            "likes": 4,
+            "__v": 0
+        },
+        {
+            "_id": "653d635d35555dd4c18c39e9",
+            "title": "I am another blog",
+            "author": "Per",
+            "url": "localhost",
+            "likes": 4,
+            "__v": 0
+        },
+        {
+            "_id": "653d64d0624f7ff7b298a292",
+            "title": "I am yet another blog",
+            "author": "Mister Minit",
+            "url": "localhost",
+            "likes": 4,
+            "__v": 0
+        }
+    ]
+
+    test("of a list with multiple blogs by the same author", () => {
+        expect(listHelper.mostBlogs(listWithMultiples)).toEqual({
+            author: "Mister Minit",
+            blogs: 2
+        })
+    })
+
+    const listWithTie = [
+        {
+            "_id": "653d623e6c9f6b487612fd23",
+            "title": "I am test blog",
+            "author": "Mister Minit",
+            "url": "localhost",
+            "likes": 4,
+            "__v": 0
+        },
+        {
+            "_id": "653d635d35555dd4c18c39e9",
+            "title": "I am another blog",
+            "author": "Per",
+            "url": "localhost",
+            "likes": 8,
+            "__v": 0
+        },
+        {
+            "_id": "653d64d0624f7ff7b298a292",
+            "title": "I am yet another blog",
+            "author": "Mister Minit",
+            "url": "localhost",
+            "likes": 4,
+            "__v": 0
+        },
+        {
+            "_id": "653d64d0g2447ff7b298g394",
+            "title": "I am unimportant",
+            "author": "Heino",
+            "url": "localhost",
+            "likes": 2,
+            "__v": 0
+        },
+        {
+            "_id": "1jd9635d355558dkc18c39e9",
+            "title": "I am important",
+            "author": "Per",
+            "url": "localhost",
+            "likes": 1,
+            "__v": 0
+        },
+    ]
+
+    test("of a list with a tie", () => {
+        const result = listHelper.mostBlogs(listWithTie)
+        expect(["Mister Minit", "Per"]).toContain(result.author)
+        expect(result.blogs).toBe(2)
+    })
+
+})
+
+describe("most likes", () => {
+    test("of an empty list", () => {
+        expect(listHelper.mostLikes([])).toEqual({})
+    })
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    test("of a list with one entry", () => {
+        expect(listHelper.mostLikes(listWithOneBlog)).toEqual({
+            author: listWithOneBlog[0].author,
+            likes: listWithOneBlog[0].likes
+        })
+    })
+
+    const listWithMultiples = [
+        {
+            "_id": "653d623e6c9f6b487612fd23",
+            "title": "I am test blog",
+            "author": "Mister Minit",
+            "url": "localhost",
+            "likes": 4,
+            "__v": 0
+        },
+        {
+            "_id": "653d635d35555dd4c18c39e9",
+            "title": "I am another blog",
+            "author": "Per",
+            "url": "localhost",
+            "likes": 4,
+            "__v": 0
+        },
+        {
+            "_id": "653d64d0624f7ff7b298a292",
+            "title": "I am yet another blog",
+            "author": "Mister Minit",
+            "url": "localhost",
+            "likes": 4,
+            "__v": 0
+        }
+    ]
+
+    test("of a list with multiple blogs by the same author", () => {
+        expect(listHelper.mostLikes(listWithMultiples)).toEqual({
+            author: "Mister Minit",
+            likes: 8
+        })
+    })
+
+    const listWithTie = [
+        {
+            "_id": "653d623e6c9f6b487612fd23",
+            "title": "I am test blog",
+            "author": "Mister Minit",
+            "url": "localhost",
+            "likes": 4,
+            "__v": 0
+        },
+        {
+            "_id": "653d635d35555dd4c18c39e9",
+            "title": "I am another blog",
+            "author": "Per",
+            "url": "localhost",
+            "likes": 8,
+            "__v": 0
+        },
+        {
+            "_id": "653d64d0624f7ff7b298a292",
+            "title": "I am yet another blog",
+            "author": "Mister Minit",
+            "url": "localhost",
+            "likes": 4,
+            "__v": 0
+        },
+        {
+            "_id": "653d64d0g2447ff7b298g394",
+            "title": "I am unimportant",
+            "author": "Heino",
+            "url": "localhost",
+            "likes": 2,
+            "__v": 0
+        }
+    ]
+
+    test("of a list with a tie", () => {
+        const result = listHelper.mostLikes(listWithTie)
+        expect(["Mister Minit", "Per"]).toContain(result.author)
+        expect(result.likes).toBe(8)
+    })
+
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,10 +1,12 @@
+var _ = require("lodash")
+
 const dummy = () => 1
 
 const totalLikes = blogs => blogs.reduce((sum, blog) => sum + blog.likes, 0)
 
 const favoriteBlog = blogs => {
     if (!blogs || blogs.length === 0) return {}
-    let favorite = {likes: 0}
+    let favorite = { likes: 0 }
     blogs.forEach(blog => {
         if (blog.likes > favorite.likes)
             favorite = blog
@@ -16,8 +18,34 @@ const favoriteBlog = blogs => {
     }
 }
 
+const mostBlogs = (blogs) => {
+    if (!blogs || blogs.length === 0)
+        return {}
+    else return _(blogs)
+        .groupBy("author")
+        .map((objs, key) => ({
+            'author': key,
+            'blogs': objs.length
+        }))
+        .maxBy(author => author.blogs)
+}
+
+const mostLikes = (blogs) => {
+    if (!blogs || blogs.length === 0)
+        return {}
+    else return _(blogs)
+        .groupBy("author")
+        .map((objs, key) => ({
+            'author': key,
+            'likes': _.sumBy(objs, 'likes')
+        }))
+        .maxBy(author => author.likes)
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
