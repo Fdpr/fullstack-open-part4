19af8dc6eac57829da2fa1586abac27618ab7e6e
Florian <70607158+Fdpr@users.noreply.github.com>
2023-10-29 21:11:39 +0200

    4.10

--- tests/http_calls.test.js
+++ tests/http_calls.test.js
@@ -22,12 +22,36 @@ test('GET /api/blogs returns all blogs', async () => {
         .expect('Content-Type', /application\/json/)
 
     expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
+}, 100000)
 
 test('blogs have id property', async () => {
     const blogs = await helper.blogsInDb()
     expect(blogs[0].id).toBeDefined()
-})
+}, 100000)
+
+test('POST api/blogs pushes blog to server and saves content', async () => {
+    const blog = {
+        title: "new Blog",
+        author: "Barbara",
+        url: "0.0.0.0",
+        likes: 4
+    }
+
+    const response = await api
+        .post('/api/blogs')
+        .send(blog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const receivedBlog = response.body
+
+    const allBlogsNow = await helper.blogsInDb()
+    expect(allBlogsNow.length).toBe(helper.initialBlogs.length + 1)
+    expect(receivedBlog.title).toBe(blog.title)
+    expect(receivedBlog.author).toBe(blog.author)
+    expect(receivedBlog.url).toBe(blog.url)
+    expect(receivedBlog.likes).toBe(blog.likes)
+}, 100000)
 
 afterAll(async () => {
     await mongoose.connection.close()
