a5a12129fb99ca70460106473be4ceb1a354f9d0
Florian <70607158+Fdpr@users.noreply.github.com>
2023-10-30 18:48:01 +0200

    4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,33 @@
 const blogsRouter = require("express").Router()
+const jwt = require("jsonwebtoken")
 const Blog = require("../models/blog")
 const User = require("../models/user")
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.slice(7)
+    }
+    return null
+}
+
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({}).populate("user", {username: 1, name: 1, id: 1})
+    const blogs = await Blog.find({}).populate("user", { username: 1, name: 1, id: 1 })
     response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-    const users = await User.find({})
-    //This is randomness to be deleted in 4.19
-    const whichUser = users[Math.floor(Math.random() * users.length)]
-    const blog = new Blog({...request.body, user: whichUser._id})
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+    const blog = new Blog({ ...request.body, user: user._id })
 
     const result = await blog.save()
 
-    whichUser.blogs = whichUser.blogs.concat(result._id)
-    await whichUser.save()
+    user.blogs = user.blogs.concat(result._id)
+    await user.save()
     response.status(201).json(result)
 })
 
